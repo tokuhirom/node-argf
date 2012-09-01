ff0bd3872dd104485a01317faea677865fa2678f
tokuhirom <tokuhirom@gmail.com>
2012-09-01 20:19:29 +0900

    refactoring

--- index.js
+++ index.js
@@ -4,35 +4,32 @@ function ARGF(args) {
     if (arguments.length === 0) {
         args = process.argv.splice(2);
     }
-    this.files = args.slice();
-    this.bufsiz = 4096;
-    this.buffer = '';
+    this.files    = args.slice();
+    this.bufsiz   = 4096;
+    this.buffer   = '';
     this.encoding = 'utf-8';
 }
 
 /**
  * read line from ARGF.
  */
-ARGF.prototype.forEach = function (code) {
-    if (this.files.length === 0) {
-        this.stream = process.stdin;
-    } else {
-        this.filename = this.files.shift();
-        this.stream = fs.createReadStream(this.filename);
-    }
-    this._forEachStream(code);
+ARGF.prototype.forEach = function (callback) {
+    this.openFirstStream();
+    this._forEachStream(callback);
 };
-ARGF.prototype._forEachStream = function (code) {
+ARGF.prototype._forEachStream = function (callback) {
     var self = this;
     this.stream.resume();
     this.stream.setEncoding(this.encoding);
     this.stream.on('data', function (chunk) {
         self.buffer += chunk;
+
+        // can you write better code?
         var b = true;
         while (b) {
             b = false;
             self.buffer = self.buffer.replace(/^([^\n]*\n)/, function (line) {
-                code(line);
+                callback(line);
                 b = true;
                 return '';
             });
@@ -41,15 +38,23 @@ ARGF.prototype._forEachStream = function (code) {
 
     this.stream.on('end', function () {
         if (self.buffer.length > 0) {
-            code(self.buffer);
+            callback(self.buffer);
             self.buffer = '';
         }
         if (self.files.length > 0) {
             self.filename = self.files.shift();
             self.stream = fs.createReadStream(self.filename);
-            self._forEachStream(code);
+            self._forEachStream(callback);
         }
     });
+};
+ARGF.prototype.openFirstStream = function () {
+    if (this.files.length === 0) {
+        this.stream = process.stdin;
+    } else {
+        this.filename = this.files.shift();
+        this.stream = fs.createReadStream(this.filename);
+    }
 };
 
 module.exports = ARGF;
